7f739cd35e5536194784387462f7d44cb733be46
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 08:53:33 -0800

    Fix: Change the background color to blue only when the mouse is hovering over the element

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 @Directive({
   selector: '[appBetterHighlight]'
   })
 export class BetterHighlightDirective implements OnInit { //implement different hooks
+
   constructor(private elRef: ElementRef, private renderer: Renderer2)  {
   }
   ngOnInit() {
-  this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
    //element, attribute, selection
    //need access to underlying element with nativeElement
   }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
+  this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  }
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+    }
 }
