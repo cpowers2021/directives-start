a204d757751b83bc54364f9eeecf3bc95821e303
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 08:46:50 -0800

    Feat: Import and utilize the BetterHighlightDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight/directive';
-
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
     BasicHighlightDirective
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
