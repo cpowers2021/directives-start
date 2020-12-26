d214854682a653bfbb80c920dccda75a126e2740
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 18:44:08 -0800

    Feat: Simplify the directive by adding variables for code to be overwritten

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -3,23 +3,25 @@ import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } f
   selector: '[appBetterHighlight]'
   })
 export class BetterHighlightDirective implements OnInit { //implement different hooks
-
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
   constructor(private elRef: ElementRef, private renderer: Renderer2)  {
   }
   ngOnInit() {
+  this.backgroundColor = this.defaultColor;
   //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
    //element, attribute, selection
    //need access to underlying element with nativeElement
   }
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   // = transparent gives an initial value
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
   //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-  this.backgroundColor = 'blue';
+  this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
    //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
-   this.backgroundColor = 'transparent';
+   this.backgroundColor = this.defaultColor;
     }
 }
